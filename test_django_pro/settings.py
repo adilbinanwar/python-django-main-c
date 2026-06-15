55b4cd1304fc7d2700217c21fd8d6f121639b668
adilbinanwar <adil004anwar@gmail.com>
2026-06-15 12:42:39 +0530

    Update ALLOWED_HOSTS with EC2 public IP

--- test_django_pro/settings.py
+++ test_django_pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5cqh@^@7g&-03mk-x-o=xht0^jfu=ujx#i4kr2q#*p@eq71=sy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-ec2-public-ip', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['3.110.224.60', 'localhost', '127.0.0.1']
 
 # Or for quick testing inside the container environment:
 ALLOWED_HOSTS = ['*']
